7edf0493d7abeee968ac788e3d355d1f27dacb88
AliHossain01 <alihossaincse01@gmail.com>
2022-05-23 13:20:09 +0600

    single parts api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,6 +28,16 @@ async function run() {
             const parts = await cursor.toArray();
             res.send(parts);
         })
+
+        app.get('/part/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const part = await partCollection.findOne(query);
+            res.send(part);
+        });
+
+
+
 
 
     }
